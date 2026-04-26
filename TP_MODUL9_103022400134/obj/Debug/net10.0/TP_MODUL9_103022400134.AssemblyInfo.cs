4d346d508240ddafffcb5d7758223adeaaa22a24
Ridho941 <firdausridho595@gmail.com>
2026-04-27 05:18:45 +0700

    Menambahkan implementasi runtime configuration

--- TP_MODUL9_103022400134/obj/Debug/net10.0/TP_MODUL9_103022400134.AssemblyInfo.cs
+++ TP_MODUL9_103022400134/obj/Debug/net10.0/TP_MODUL9_103022400134.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP_MODUL9_103022400134")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76bec5ebfd8a66745c7b8de796056f7e3a73cf14")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP_MODUL9_103022400134")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP_MODUL9_103022400134")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
